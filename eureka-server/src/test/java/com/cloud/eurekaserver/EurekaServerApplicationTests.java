88cb102a13b0f5b812a0aef41da32440a68664ec
andychen <strikecbu@gmail.com>
2022-07-14 22:26:43 +0800

    [CloudBank] Add all microservices helm files and deploy helm config

--- eureka-server/src/test/java/com/cloud/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/cloud/eurekaserver/EurekaServerApplicationTests.java
@@ -3,12 +3,17 @@ package com.cloud.eurekaserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.nio.charset.Charset;
+import java.util.Base64;
+
 
 class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		byte[] decode = Base64.getDecoder()
+				.decode("Z2hwXzNiUVl4VUZCNGw1NWczdFhaSzNDYklhWE1QVUx0dTJGbVFKbQ==");
+		System.out.println(new String(decode, Charset.defaultCharset()));
 	}
 
 }
